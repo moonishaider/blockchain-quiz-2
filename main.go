67fd06808ece1ec08450b59ba4396690759375bf
moonishaider <moonishaider12@gmail.com>
2024-03-04 22:48:10 +0500

    add go files

--- main.go
+++ main.go
@@ -6,16 +6,13 @@ import (
 	
 )
 
-// Block represents a data structure for your blocks
 type Block struct {
 	ID   int
 	Data string
 }
 
-// BlockDatabase is a simple in-memory database for blocks
 var BlockDatabase []Block
 
-// DisplayAllBlocks prints all the blocks in the database
 func DisplayAllBlocks() {
 	fmt.Println("All Blocks:")
 	for _, block := range BlockDatabase {
@@ -24,7 +21,6 @@ func DisplayAllBlocks() {
 	fmt.Println()
 }
 
-// NewBlock creates a new block and adds it to the database
 func NewBlock(data string) {
 	newID := len(BlockDatabase) + 1
 	newBlock := Block{ID: newID, Data: data}
@@ -32,7 +28,7 @@ func NewBlock(data string) {
 	fmt.Printf("New Block Created - ID: %d, Data: %s\n\n", newID, data)
 }
 
-// ModifyBlock updates the data of an existing block in the database
+
 func ModifyBlock(id int, newData string) {
 	for i := range BlockDatabase {
 		if BlockDatabase[i].ID == id {
@@ -45,7 +41,7 @@ func ModifyBlock(id int, newData string) {
 }
 
 func main() {
-	// Example usage
+	
 	NewBlock("Block 1 Data")
 	NewBlock("Block 2 Data")
 	DisplayAllBlocks()
